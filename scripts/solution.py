5b907b514eeb817a08f740e7bf367c897c2fd826
entity126 <ayabongamhlungu126@gmail.com>
2025-06-17 03:22:03 +0200

    solution python file

---
+++ scripts/solution.py
@@ -0,0 +1,7 @@
+class solution():
+    def negl(self):
+        return 0
+    def act_negl(self):
+        return 0
+    
+print("This is a dummy solution class for testing purposes. the script will be written as the peer reviwing continues.")
